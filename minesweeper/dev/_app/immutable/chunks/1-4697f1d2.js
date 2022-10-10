5490db28464c76eda1a083575c55feb0d30f4baf
SuperFluffyGame <81885757+SuperFluffyGame@users.noreply.github.com>
2022-10-09 19:46:35 -0500

    fix?

--- minesweeper/dev/_app/immutable/chunks/1-4697f1d2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-d5276bd9.js";export{t as component};
---
+++ minesweeper/dev/_app/immutable/chunks/3-b179f463.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/game/_page.svelte-bc4bb414.js";export{t as component};
